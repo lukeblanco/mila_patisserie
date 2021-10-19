0e4e70e79cfd753a0019a12c417a287c88f58438
Anto Pulvirenti <antopulvirenti@hotmail.com>
2021-10-18 22:41:03 -0300

    First components

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-center">Productos</h1>
+    <div class="mt-1 w-40 m-auto border border-red-500" />
+    <div class="grid grid-cols-4">
+      <div v-for="producto in productos" :key="producto.name">
+        <p>{{ producto.name }}</p>
+        <p>{{ producto.price }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import productos from "./data.json";
+export default {
+  name: "Home",
+  data() {
+    return {
+      productos,
+    };
+  },
+  methods: {},
+};
+</script>

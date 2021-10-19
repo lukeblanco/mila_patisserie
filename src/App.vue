817feb82c0af96cc5248c6f40f8e487a67be9c84
Anto Pulvirenti <antopulvirenti@hotmail.com>
2021-10-18 22:39:06 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="max-w-5xl m-auto" id="app">
+    <Header/>
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>

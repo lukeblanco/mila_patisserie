3ddf817d6e0fb5cfd35998fa23dd88e38d20ce9b
julizas <julianzaselsky@gmail.com>
2021-11-08 19:33:23 -0300

    buy products flow

--- src/views/Carrito.vue
+++ src/views/Carrito.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
      <div class="grid grid-cols-4 gap-5">
       <div v-for="producto in products" :key="producto.name">
         <h2 class="font-bold mb-1">{{ producto.name }}</h2>
@@ -7,14 +8,42 @@
         <DeleteProduct :product="producto"/>
       </div>
     </div>
+    <button @click="buyProducts(products)">Comprar Productos</button>
+    <div v-if="wasBuyed"> La compra fue realizada con exito </div>
+  </div>
 </template>
 
 <script>
     import { mapGetters } from "vuex";
+    import axios from "axios";
     import DeleteProduct from "../components/deleteProduct.vue";
     export default {
+    data() {
+    return {
+      wasBuyed: null,
+    };
+     },
     computed: {
         ...mapGetters({ products: "getCartProducts" }),
+        
+    },
+    methods: {
+      buyProducts: async function(products) {
+        console.log("hola")
+        try{
+        const purchaseData = {
+          products: products,
+          userId: Math.random()*100
+        } 
+        const purchase = await axios.post("https://61774b8c9c328300175f58a1.mockapi.io/api/Orders",purchaseData)
+        console.log(purchase,"purchase");
+        this.$store.dispatch("empty");
+        this.wasBuyed = true;
+        }
+        catch(err){
+          console.log(err)
+        }
+      }
     },
       components:{
       DeleteProduct
